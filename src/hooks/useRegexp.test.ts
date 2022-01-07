980e2091d882d679c0b5db87719c76c8b5c1cb58
Christian Rudh <christian@rudh.se>
2022-01-07 13:07:20 +0100

    Added ignore case tests for useRegexp

--- src/hooks/useRegexp.test.ts
+++ src/hooks/useRegexp.test.ts
@@ -2,7 +2,7 @@ import { renderHook } from "@testing-library/react-hooks";
 import { useRegexp } from "./useRegexp";
 
 test("should work with an empty regexp", () => {
-  const { result } = renderHook(() => useRegexp("", "g"));
+  const { result } = renderHook(() => useRegexp("", { g: true, i: false }));
 
   const [regexp, errorMessage] = result.current;
 
@@ -11,7 +11,7 @@ test("should work with an empty regexp", () => {
 });
 
 test("should not work with an incomplete regexp", () => {
-  const { result } = renderHook(() => useRegexp("\\", "g"));
+  const { result } = renderHook(() => useRegexp("\\", { g: true, i: false }));
 
   const [regexp, errorMessage] = result.current;
 
@@ -19,4 +19,26 @@ test("should not work with an incomplete regexp", () => {
   expect(regexp).toBeUndefined();
 
   expect(errorMessage?.includes("Invalid regular expression")).toEqual(true);
+});
+
+test("should work with an normal regexp", () => {
+  const { result } = renderHook(() =>
+    useRegexp("\\d{3}", { g: true, i: false })
+  );
+
+  const [regexp, errorMessage] = result.current;
+
+  expect(errorMessage).toBeUndefined();
+  expect(regexp).toBeDefined();
+});
+
+test("should work with an normal regexp and ignore case", () => {
+  const { result } = renderHook(() =>
+    useRegexp("\\d{3}", { g: true, i: true })
+  );
+
+  const [regexp, errorMessage] = result.current;
+
+  expect(errorMessage).toBeUndefined();
+  expect(regexp).toBeDefined();
 });
